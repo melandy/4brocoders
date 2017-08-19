aa3d4a8f6554d666697ea5d10ad827051d0e0a96
melandy <andy.melentyev@gmail.com>
2017-08-19 16:33:36 +0300

    broc component v0.0.2

--- broc/broccomponent.js
+++ broc/broccomponent.js
@@ -7,47 +7,61 @@ class BrocComponent extends HTMLElement {
     this.numRows = +this.getAttribute("rows") || 1;
     this.numCols = +this.getAttribute("cols") || 1;
 
-    this.shadow = this.createShadowRoot();
-
+    const shadow = this.createShadowRoot();
+  
     const localDocument = document.currentScript.ownerDocument;
     const tmpl = localDocument.getElementById("tmpl");
-    this.shadow.appendChild(tmpl.content.cloneNode(true));
+    shadow.appendChild(tmpl.content.cloneNode(true));
+  
+    this.rootDiv = shadow.querySelector(".broc__component");
+    this.blueTbl = shadow.querySelector(".broco__table");
+  
+    this.deleteRowButton = this.rootDiv.querySelector(".broco__button-del.row");
+    this.deleteColumnButton = this.rootDiv.querySelector(".broco__button-del.col");
 
-    this.rootDiv = this.shadow.querySelector(".broc__component");
-    this.blueTbl = this.shadow.querySelector(".broco__table");
+    this.addRowButton = this.rootDiv.querySelector(".broco__button-add.row");
+    this.addColumnButton = this.rootDiv.querySelector(".broco__button-add.col");
   }
 
   createTable() {
+    const tbody = document.createElement("tbody");
     for (let i = 0; i < this.numRows; i++) {
-      this.makeRowForTable(this.numCols);
+      const trow = this.makeRowForTable(this.numCols);
+      tbody.appendChild(trow);
     }
+    this.blueTbl.appendChild(tbody);
+
     this.blueTbl.onmousemove = this.moveButton();
-    this.blueTbl.onmouseenter = this.showDelButton(true);
-    this.blueTbl.onmouseleave = this.showDelButton(false);
+    this.blueTbl.onmouseenter = this.showDelButton();
+    this.blueTbl.onmouseleave = this.hideDelButton();
   }
 
   makeRowForTable(col) {
-    let tr = this.blueTbl.appendChild(document.createElement("tr"));
+    const tr = document.createElement("tr");
     for (let i = 0; i < col; i++) {
       let td = tr.appendChild(document.createElement("td"));
       td.className = "broco__table-elem";
     }
+    return tr
   }
 
   addRow() {
     return ()=> {
-      this.makeRowForTable(this.numCols);
+      const tbody = this.blueTbl.querySelector("tbody");
+      const trow = this.makeRowForTable(this.numCols);
+      tbody.appendChild(trow);
       this.numRows++;
     }
   }
 
   deleteRow() {
     return ()=> {
-      let tr = this.blueTbl.lastChild.rowIndex;
+      const tbody = this.blueTbl.querySelector("tbody");
+      const tr = tbody.lastChild.rowIndex;
       if (tr > 0) {
-        this.blueTbl.deleteRow(tr);
+        tbody.deleteRow(tr);
         this.numRows--;
-        this.rootDiv.querySelector(".broco__button-del.row").classList.add("broco__button-del-hidden");
+        this.deleteRowButton.classList.add("broco__button-del-hidden");
       }
     }
   }
@@ -67,62 +81,59 @@ class BrocComponent extends HTMLElement {
     return () => {
       let trs = this.blueTbl.querySelectorAll("tr");
       if (this.numCols > 1) {
-      for (let tr of trs) {
-        tr.lastChild.remove();
-      }
-      this.numCols--;
-      this.rootDiv.querySelector(".broco__button-del.col").classList.add("broco__button-del-hidden");
+        for (let tr of trs) {
+          tr.lastChild.remove();
+        }
+        this.numCols--;
+        this.deleteColumnButton.classList.add("broco__button-del-hidden");
       }
     }
   }
 
   addEvent(arr) {
-    for (let item of arr) {
-      let btn = this.rootDiv.querySelector(item.btn);
-      btn[item.event] = item.cb;
-    }
+    arr.map(item => {
+      const btn = item.btn;
+      if (btn) btn[item.event] = item.cb;
+    });
   }
 
   createButtonsEvents() {
     const buttons = [
-      { btn: ".broco__button-del.col", event: "onclick",    cb: this.deleteCol() },
-      { btn: ".broco__button-del.col", event: "onmouseenter", cb: this.showDelButton(true) },
-      { btn: ".broco__button-del.col", event: "onmouseleave", cb: this.showDelButton(false) },
-      { btn: ".broco__button-del.row", event: "onclick",    cb: this.deleteRow() },
-      { btn: ".broco__button-del.row", event: "onmouseenter", cb: this.showDelButton(true) },
-      { btn: ".broco__button-del.row", event: "onmouseleave", cb: this.showDelButton(false) },
-      { btn: ".broco__button-add.col", event: "onclick",    cb: this.addCol() },
-      { btn: ".broco__button-add.row", event: "onclick",    cb: this.addRow() }
+      { btn: this.deleteColumnButton, event: "onclick",      cb: this.deleteCol() },
+      { btn: this.deleteColumnButton, event: "onmouseenter", cb: this.showDelButton() },
+      { btn: this.deleteColumnButton, event: "onmouseleave", cb: this.hideDelButton() },
+      { btn: this.deleteRowButton,    event: "onclick",      cb: this.deleteRow() },
+      { btn: this.deleteRowButton,    event: "onmouseenter", cb: this.showDelButton() },
+      { btn: this.deleteRowButton,    event: "onmouseleave", cb: this.hideDelButton() },
+      { btn: this.addColumnButton,    event: "onclick",      cb: this.addCol() },
+      { btn: this.addRowButton,       event: "onclick",      cb: this.addRow() }
     ];
     this.addEvent(buttons);
   }
 
-  showDelButton(visible) {
+  showDelButton() {
     return (e) => {
       e.stopPropagation();
-      const btnsDel = this.rootDiv.querySelectorAll(".broco__button-del");
-      if (visible) {
-        for (let btn of btnsDel) {
-          if (
-            (btn.classList.contains("row") && this.numRows > 1) ||
-            (btn.classList.contains("col") && this.numCols > 1)
-          ) {
-            btn.classList.remove("broco__button-del-hidden");
-          }
-        }
-      } else {
-        for (let btn of btnsDel) {
-          btn.classList.add("broco__button-del-hidden");
-        }
+      if (this.numCols > 1) {
+        this.deleteColumnButton.classList.remove("broco__button-del-hidden");
+      }
+      if (this.numRows > 1) {
+        this.deleteRowButton.classList.remove("broco__button-del-hidden");
       }
     }
   }
 
+  hideDelButton() {
+    return (e) => {
+      e.stopPropagation();
+      this.deleteColumnButton.classList.add("broco__button-del-hidden");
+      this.deleteRowButton.classList.add("broco__button-del-hidden");
+    }
+  }
+  
   moveButton() {
     return (e) => {
       e.stopPropagation();
-      const btnDelRow = this.rootDiv.querySelector(".broco__button-del.row");
-      const btnDelCol = this.rootDiv.querySelector(".broco__button-del.col");
 
       const startPositionLeft = this.blueTbl.getBoundingClientRect().left;
       const startPositionTop = this.blueTbl.getBoundingClientRect().top;
@@ -130,8 +141,8 @@ class BrocComponent extends HTMLElement {
       const elemDelRow = parseInt((e.pageY - startPositionTop - 4) / 52) * 52;
       const elemDelCol = parseInt((e.pageX - startPositionLeft - 4) / 52) * 52;
 
-      btnDelRow.style.transform = `translateY(${elemDelRow}px)`;
-      btnDelCol.style.transform = `translateX(${elemDelCol}px)`;
+      this.deleteRowButton.style.transform = `translateY(${elemDelRow}px)`;
+      this.deleteColumnButton.style.transform = `translateX(${elemDelCol}px)`;
     }
   }
 
